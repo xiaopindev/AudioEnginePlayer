3f08f62532066d50cb62b2c5d3d8a537ec8e91e9
xiaopin <251089096@qq.com>
2024-07-26 16:35:24 +0800

    add widgets

---
+++ ios/Widgets/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Widgets
+//
+//  Created by xiaopin on 2024/7/26.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ ios/Widgets/WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by xiaopin on 2024/7/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsLiveActivity()
+    }
+}
